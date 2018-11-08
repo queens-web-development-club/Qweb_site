13a08d9b8b5e42c048b899ca1d0dff49cdde7ef4
heyselt <15tjh2@queensu.ca>
2018-11-07 20:38:29 -0500

    First test

--- page-templates/page-contact.php
+++ page-templates/page-contact.php
@@ -4,6 +4,7 @@ bb_add_body_classes('contact');
 the_post();
 get_header();
 ?>
+
 <div class='container-fluid background-texture contact h-75'>
   <div class="row h-100">
     <div class="col-md-12">
@@ -17,8 +18,15 @@ get_header();
       </div>
     </div>
   </div>
+</div>
+</header>
 
+<body>
+<div class=Contact-Setion>
+  <h3>Get in Touch</h3>
+  <h5>Talk to <span style="color: #5f084b">our team</span> of experienced students to take your project from concept to <span style="color:#006f71">completion</span></h5>
 </div>
 
-</header>
+</body>
+
 <?php get_footer(); ?>
