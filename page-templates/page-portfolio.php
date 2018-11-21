1746e4b44b17634543ac688219fbd9dbeb58b2d4
Julia McPolin <juliamcpolin@Jules-MacBook-Pro.local>
2018-11-21 17:09:48 -0500

    added photos to portfolio page, added css

--- page-templates/page-portfolio.php
+++ page-templates/page-portfolio.php
@@ -21,4 +21,334 @@ get_header();
 </div>
 
 </header>
-<?php get_footer(); ?>
+
+
+<!--Started here-->
+
+
+<div class="Websites">
+  <p> <span style="color:#505050">10 </span>Websites. </p>
+</div>
+
+<div class="CLUBS">
+
+  <p>CLUBS</p>
+
+    <img src="/wp-content/themes/Qweb_site/media/line-2.png"
+     class="Line-2">
+
+  <img src="/wp-content/themes/Qweb_site/media/line.png"
+     class="Line">
+
+  </div>
+
+<div class="wrapper">
+
+  <!--box a -->
+    <div class="box word-image-574-1024x503">
+
+    <img src="/wp-content/themes/Qweb_site/media/
+word-image-574-1024-x-503.png" class="box word-image-574-1024x503">
+   </div>
+<!--/wp-content/themes/Qweb_site/media/technologies-banner.png-->
+<!--box b -->
+  <div class="box word-image-586-1024x506">
+
+    <img src= "/wp-content/themes/Qweb_site/media/word-image-586-1024-x-506.png" class="box word-image-586-1024x506">
+
+  </div>
+
+<!--box c -->
+  <div class="box word-image-580-1024x502">
+
+    <img src= "/wp-content/themes/Qweb_site/media/word-image-580-1024-x-502.png" class="box word-image-580-1024x502">
+    
+  </div>
+
+  <!--box d -->
+  <div class="box word-image-583-1024x502">
+
+    <img src="/wp-content/themes/Qweb_site/media/word-image-583-1024-x-502.png" class="box word-image-583-1024x502">
+    
+  </div>
+
+  <!--box e -->
+  <div class="box word-image-582-1024x504">
+
+    <img src="/wp-content/themes/Qweb_site/media/word-image-582-1024-x-504.png" class="box word-image-582-1024x504">
+    
+  </div>
+
+  <!--box f -->
+  <div class="box word-image-581-1024x504"">
+
+    <img src="/wp-content/themes/Qweb_site/media/word-image-581-1024-x-504.png" class="box word-image-581-1024x504">
+    
+  </div>
+
+
+</div>
+
+<div class = "Technology-Banner">
+<!-- png -->
+
+<img src="/wp-content/themes/Qweb_site/media/technologies-banner.png"
+
+     class="Technology-Banner">
+</div>
+
+<div class="BUSINESS">
+  <p>BUSINESSES</p>
+
+<div class="businessLine">
+</div>
+
+</div>
+
+<div class="wrapper">
+
+  <!--box g -->
+  <div class="box word-image-573-1024x502">
+
+    <img src=/wp-content/themes/Qweb_site/media/word-image-573-1024-x-502.png" class="box word-image-573-1024x502">
+
+  </div>
+
+  <!--box h -->
+  <div class="word-image-578-1024x503">
+
+    <img src="/wp-content/themes/Qweb_site/media/word-image-578-1024-x-503.png" class="box word-image-578-1024x503">
+   
+  </div>
+
+  <!--box i -->
+  <div class="box word-image-584-1024x503">
+
+    <img src= "/wp-content/themes/Qweb_site/media/word-image-584-1024-x-503.png" class="box word-image-584-1024x503">
+    
+  </div>
+
+  <!--box j -->
+  <div class="box word-image-579-1024x501">
+
+    <img src= "/wp-content/themes/Qweb_site/media/word-image-579-1024-x-501.png" class="box word-image-579-1024x501">
+    
+  </div>
+    
+  </div>
+</div>
+
+
+
+<div class="CLIENTS">
+  <p>CLIENTS</p>
+
+<div class="clientLine">
+</div>
+
+
+</div>
+
+<div class="arrows">
+
+<!-- png -->
+
+<img src="/wp-content/themes/Qweb_site/media/right-arrow-icon-27.png"
+     class="left-arrow-icon-27">
+
+<img src="/wp-content/themes/Qweb_site/media/client-2.png"
+     class="client-2">
+
+<img src="/wp-content/themes/Qweb_site/media/right-arrow-icon-27.png"
+     class="right-arrow-icon-27" style = "float:right">
+</div>
+
+
+
+<!-- footer -->
+
+  <hr>
+  <br>
+  <div class="row h-100">
+    <div class="col-md-4">
+          <div class="text-center email-copyright">
+            <p>qwdc@clubs.queensu.ca</p>
+        </div>
+    </div>
+    <div class="col-md-4">
+          <div class="text-center email-copyright">
+            <p>Copyright qweb.tech All Rights Reserved</p>
+        </div>
+    </div>
+    <div class="col-md-4">
+          <div class="text-center email-copyright">
+            <p>99 University Ave Kingston, ON</p>
+        </div>
+    </div>
+  </div>
+  <br>
+
+  <div class="row h-100">
+    <div class="col-md-12 text-center">
+          <img src="/wp-content/themes/Qweb_site/media/facebook-icon-dark.png">
+          <img src="/wp-content/themes/Qweb_site/media/insta-icon-dark.png">
+          <img src="/wp-content/themes/Qweb_site/media/logo-dark.png">
+          <img src="/wp-content/themes/Qweb_site/media/linkedin-icon-dark.png">
+          <img src="/wp-content/themes/Qweb_site/media/mail-icon-dark.png">
+    </div>
+  </div>
+
+</div>
+<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
+</body>
+</html>
+
+<style>
+.Technologies-Banner{
+  width: 100%;
+  padding-bottom:80px;
+ /* padding-top: 150px;*/
+  padding-left: 31px;
+  padding-right: 76px;
+
+}
+
+.wrapper {
+  display: grid;
+  grid-template-columns: 812px 356px;
+  grid-gap: 47px;
+  margin: 116px;
+}
+
+.box {
+  
+  background-color: pink;
+  width:812px;
+  height: 356px;
+}
+
+.Websites {
+  /*width:100%;*/
+  height: 98px;
+  object-fit: contain;
+  /*font-family: AvenirNext, serif;*/
+  font-size: 72px;
+  font-weight: 500;
+  font-style: normal;
+  font-stretch: normal;
+  line-height: normal;
+  letter-spacing: normal;
+  text-align: right;
+  color: #5f084b;
+  margin-right:33px;
+
+}
+
+.Websites .text-style-1 {
+  color: #505050;
+}
+
+.Websites .text-style-2 {
+  color: #5a5a5a;
+}
+
+.Line-2, .businessLine, .clientLine {
+  width: 784px;
+  object-fit: contain;
+  border: solid 5px #006f71;
+  margin-right:33px;
+  position:absolute;
+  z-index:2;
+}
+
+.businessLine {
+  border: solid 5px #5f084b;
+}
+
+.clientLine {
+  border: solid 5px #2f3c5e;
+}
+.Line {
+  width: 1815px;
+  height:2px;
+  object-fit: contain;
+  border: solid 2px #979797;
+  position:absolute;
+  z-index:1;
+}
+
+.wrapper {
+  display: grid;
+  grid-template-columns: 812px 356px;
+  grid-gap: 47px;
+  margin: 116px;
+}
+
+.box {
+  
+  background-color: pink;
+  width:812px;
+  height: 356px;
+
+}
+
+.CLUBS, .BUSINESS, .CLIENTS {
+  width: 399px;
+  height: 70px;
+  object-fit: contain;
+  /*font-family: AvenirNext;*/
+  font-size: 48px;
+  font-weight: normal;
+  font-style: normal;
+  font-stretch: normal;
+  line-height: normal;
+  letter-spacing: normal;
+  color: #006f71;
+  margin-left:48px;
+
+}
+
+.BUSINESS {
+  color: #5f084b;
+}
+
+.CLIENTS {
+  color: #2f3c5e;
+}
+
+.client-2 {
+  width: 200px;
+  height: 60px;
+  object-fit: contain;
+}
+
+.right-arrow-icon-27 {
+  width: 109px;
+  height: 109px;
+  object-fit: contain;
+  float:right;
+}
+
+.left-arrow-icon-27 {
+  width: 109px;
+  height: 109px;
+  object-fit: contain;
+  transform: rotate(180deg);
+
+}
+.client-content {
+  position:absolute;
+  width:100%;
+  margin-right:100px;
+  padding-right:100px;
+
+
+}
+
+
+
+</Style>
+
+
